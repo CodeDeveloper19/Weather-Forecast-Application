c77ded167127ef7735cce913469870f9398ebc1f
Okoli Akachukwu <93165724+CodeDeveloper19@users.noreply.github.com>
2022-04-04 09:38:13 +0100

    Delete Weather Forecast App Test directory

--- Weather Forecast App Test/credits.js
+++
@@ -1,7 +0,0 @@
-var text = document.getElementById("text");
-var credits = document.getElementById("credits2");
-var close = document.getElementById("closecontainer");
-
-text.addEventListener('click', () => credits.style.display = "flex")
-
-close.addEventListener('click', () => credits.style.display = "none")
